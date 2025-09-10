e275ace488d661f4268a98a62f3da97492ff067c
CainHappyfish <280971620@qq.com>
2025-09-10 15:50:55 +0800

    fix: 修复无法访问未完成页面的问题

--- blog/src/pages/Friends.vue
+++ blog/src/pages/Friends.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { DotLottieVue } from '@lottiefiles/dotlottie-vue'
+import loadingAnimation from '@/lottie/loading.json'
 
+const size = ref(300)
 </script>
 
 <template>
   <div class="friends-page">
+    <div class="loading-animation">
+      <DotLottieVue 
+        :style="{ height: `${size}px`, width: `${size}px` }" 
+        autoplay 
+        loop 
+        :data="loadingAnimation" 
+      />
+    </div>
     <h1>🚧 施工中</h1>
   </div>
 </template>
